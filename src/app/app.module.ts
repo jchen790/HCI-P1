5c5302f9cdbde6fa470078ef23b9a66263d652d3
jchen790 <jchen790@gmail.com>
2018-02-04 02:10:09 -0500

    adding basic ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { DropdownComponent } from './dropdown/dropdown.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DropdownComponent
   ],
   imports: [
     BrowserModule
